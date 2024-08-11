13e9f28c1cf77076ea1de65b4a81978968054f28
Mayur <madankarmayur5@gmail.com>
2024-08-11 23:06:05 +0530

    swagger,error handling,CORS

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //1.import expres (third party import)
 import  express from 'express';
 import swagger from "swagger-ui-express";
+import cors from 'cors';
 
 //internal import 
 import productRouter from './src/features/product/product.routes.js'
@@ -16,13 +17,31 @@ import apiDocs from "./swagger.json"assert{type:'json'};
 //2.create serv`er
 const server=express();
 
-// server.use(express.json());
+//CORS Policy Configuration
+var corsOptions={
+    origin:'http://localhost:5500'
+}
+
+server.use(cors(corsOptions));
+
+// server.use((req,res,next)=>{
+//     res.header('Access-Control-Allow-Origin','http://localhost:5500');
+//     res.header('Access-Control-Allow-Headers','*');
+//     res.header('Access-Control-Allow-Methods','*');
+//     //return ok for preflight request
+//     if(req.method=="OPTIONS"){
+//         return res.sendStatus(200);
+//     }
+//     next();
+// })
+
+server.use(express.json());
 
 //to parse the data from JSON format to req.body
 server.use(bodyParser.json());
 
 //swagger to render the API Documentation
-server.use('/api-Doc',swagger.serve,swagger.setup(apiDocs)); // here swagger.serve create the API
+server.use('/api-docs',swagger.serve,swagger.setup(apiDocs)); // here swagger.serve create the API
 
 
 //for all APIs of product,redirect to product productRouter
@@ -38,7 +57,13 @@ server.get('/',(req,res)=>{
     res.send("Welcome to Ecommerce APIs")
 })
 
-//4.specify port
+//if any one routes or api not found from above then it runs -> 4.middleware
+//4.Middleware to hande 404 requests
+server.use((req,res)=>{
+    res.status(404).send("API IS NOT FOUND , Please check our API Documentation for more information at localhost:3000/api-docs")
+})
+
+//5.specify port
 server.listen(3000,()=>{
     console.log('server is listening at port 3000');
 })
