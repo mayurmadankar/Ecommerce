2b40fc937a6e9a608b3d8dbfdd334465230f2554
Mayur <madankarmayur5@gmail.com>
2024-08-25 00:55:07 +0530

    transactions

--- src/features/order/order.repository.js
+++ src/features/order/order.repository.js
@@ -1,16 +1,51 @@
-import { ObjectId } from "mongodb";
+import { ObjectId, Timestamp } from "mongodb";
 import { getDB } from "../../config/mongodb.js";
+import OrderModel from "./order.model.js";
+import { ApplicationError } from "../../middleware/applicantionError.middleware.js";
 
 export default class OrderRepository {
   constructor() {
     this.collection = "orders";
   }
   async placeOrder(userId) {
-    //1. Get cartItems and calculate the total amounnt.
-    await this.getTotalAmount(userId);
-    //2. create an order record.
-    //3. Reduce the stock.
-    //4. Clear the cart Items.
+    try {
+      const db = getDB();
+
+      //1. Get cartItems and calculate the total amounnt.
+      const items = await this.getTotalAmount(userId);
+      console.log(items);
+      const finalTotalAmount = items.reduce(
+        (acc, item) => acc + item.totalAmount,
+        0
+      );
+      console.log(finalTotalAmount);
+
+      //2. create an order record.
+      const newOrder = new OrderModel(
+        new ObjectId(userId),
+        finalTotalAmount,
+        new Date()
+      );
+      db.collection(this.collection).insertOne(newOrder);
+
+      //3. Reduce the stock.(means reduce stock from products)
+      for (let item of items) {
+        await db
+          .collection("products")
+          .updateOne(
+            { _id: item.productID },
+            { $inc: { stock: -item.quantity } }
+          );
+      }
+
+      //4. Clear the cart Items.
+      await db.collection("cartItems").deleteMany({
+        userId: new ObjectId(userId)
+      });
+    } catch (err) {
+      console.log(err);
+      throw new ApplicationError("Something went wrong in placeOrder", 500);
+    }
   }
   async getTotalAmount(userId) {
     const db = getDB();
@@ -44,5 +79,6 @@ export default class OrderRepository {
       ])
       .toArray();
     console.log(items);
+    return items;
   }
 }
