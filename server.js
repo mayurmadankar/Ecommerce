31cf801b1839413823540a992e59a28b3b14e512
Mayur <madankarmayur5@gmail.com>
2024-09-17 16:25:06 +0530

    assert

--- server.js
+++ server.js
@@ -12,8 +12,10 @@ import userRouter from "./src/features/user/user.routes.js";
 //jwtAuth for whenever user want information they want to login through token
 import jwtAuth from "./src/middleware/jwt.middleware.js";
 import cartRouter from "./src/features/cart/cartitems.router.js";
+import apiDocs from "./swagger.json";
+import fs from "fs";
 
-import apiDocs from "./swagger.json" assert { type: "json" };
+const apiDocs = JSON.parse(fs.readFileSync("./swagger.json", "utf-8"));
 
 import loggerMiddleware from "./src/middleware/logger.middleware.js";
 import { errorHandlerMiddleware } from "./src/middleware/applicantionError.middleware.js";
